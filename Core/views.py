c9584b6803b5c02a3ab5ed9c0d61beee6242b004
fosquito <fosquitopirex@gmail.com>
2019-03-19 21:48:13 +0000

    nova pasta de tabelas.

--- Core/views.py
+++ Core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
+from Users.models import User
 
 # Create your views here.
 
@@ -9,6 +10,6 @@ from django.contrib.auth.models import Permission
 def Home(request):
         return render(request,
                       'main/home.html',
-                      context={"permissions":
-                               Permission.objects.values('name').filter(
-                                       group__user=request.user).distinct()})
+                      context={"permissions":Permission.objects.values('name').filter(group__user=request.user).distinct(),
+					  "users":User.objects.all(),
+					  "groups":Group.objects.all()})

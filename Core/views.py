2e2562a7427281001faa29a1e03a8ed1d89aa6f9
fosquito <fosquitopirex@gmail.com>
2019-03-25 20:48:38 +0000

    Menu com user, minhas frases, meus grupos, meus padroes.

--- Core/views.py
+++ Core/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Permission
+from Users.models import User
+from Activities.models import Sentence, Group, Pattern
 
 # Create your views here.
 
 
 @login_required
 def Home(request):
-        return render(request,
-                      'main/home.html',
-                      context={"permissions":
-                               Permission.objects.values('name').filter(
-                                       group__user=request.user).distinct()})
+        return render(request,'main/home.html',
+        context={"permissions":Permission.objects.values('name').filter(group__user=request.user).distinct(),
+        "users":User.objects.all(),
+        "sentences":Sentence.objects.all().filter(userid=request.user),
+        "groups":Group.objects.all().filter(userid=request.user),
+        "patterns":Pattern.objects.all().filter(userid=request.user)})
